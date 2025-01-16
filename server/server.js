98338201695d50505af0a6279bf03a837b53dd55
TheVinayVK <156287964+TheVinayVK@users.noreply.github.com>
2025-01-16 15:51:23 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:3000' })); // Allow React app to connect
+app.use(cors({ origin: 'https://todoslistme.netlify.app/' })); // Allow React app to connect
 app.use(express.json());
 
 // Connect to MongoDB
